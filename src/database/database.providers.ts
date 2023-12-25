594f1a940e646360c9a39e75dbfa758d153c4bda
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-25 21:08:59 +0200

    feat: :sparkles: [BOOT-108] Connect to database locally
    BOOT-108

---
+++ src/database/database.providers.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from 'sequelize-typescript';
+import { User } from './models/User.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+
+export const databaseProviders = [{
+  provide: 'SEQUELIZE',
+  useFactory: async () => {
+    const sequelize = new Sequelize({
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      dialect: 'postgres',
+    });
+    sequelize.addModels([User]);
+    await sequelize.sync();
+    return sequelize;
+  },
+}];
