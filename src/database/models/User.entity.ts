568ce0efeb9dddf64da434b9a84f4f47d410177b
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-25 15:37:41 +0200

    feat: :sparkles: Add user entity
    #BOOT-108

---
+++ src/database/models/User.entity.ts
@@ -0,0 +1,64 @@
+import { Table, Column, Model } from 'sequelize-typescript';
+
+@Table
+export class User extends Model {
+  @Column({
+    unique: true
+  })
+  uuid: string;
+
+  @Column({
+    unique: true
+  })
+  email: number;
+
+  @Column({
+    defaultValue: false
+  })
+  isEmailPrivate: boolean;
+
+  @Column
+  newEmail?: string;
+
+  @Column
+  password: string;
+
+  @Column
+  fullName: string;
+
+  @Column({
+    values: ['male', 'female']
+  })
+  gender: string;
+
+  @Column
+  dateOfBirth: Date;
+
+  @Column
+  createdAt: Date;
+
+  @Column
+  updatedAt: Date;
+
+  @Column
+  otpCode?: number;
+
+  @Column
+  otpCreatedAt?: Date;
+
+  @Column({
+    defaultValue: false
+  })
+  isVerified: boolean;
+
+  @Column({
+    defaultValue: 0
+  })
+  likeCounter: number;
+
+  @Column({
+    defaultValue: 0
+  })
+  commentCounter: number;
+
+}
