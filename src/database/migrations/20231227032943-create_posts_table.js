2ba7d2ceb7c6cdfa63faa9b18810afc7eccda9d3
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-27 05:42:06 +0200

    feat: :sparkles: [BOOT-108] Create '20231227032943-create_posts_table' migration

---
+++ src/database/migrations/20231227032943-create_posts_table.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('posts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      likesCounter: {
+        type: Sequelize.INTEGER
+      },
+      commentsCounter: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+      }
+    })
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('posts')
+  }
+};
