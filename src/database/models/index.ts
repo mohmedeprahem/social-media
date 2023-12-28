db63e1e38d6f1b75c358feb95e24ccd2dbac860b
mohmedeprahem <m.eprahem.168@gmail.com>
2023-12-28 06:45:55 +0200

    refactor: :building_construction: [BOOT-108] Change index extension to typescript

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -3,11 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config.ts')[env];
-const db = {};
+const config = require(__dirname + '/../config.js')[env];
+const db: any = {};
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -17,7 +16,7 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
+  .filter((file: string) => {
     return (
       file.indexOf('.') !== 0 &&
       file !== basename &&
@@ -25,7 +24,7 @@ fs
       file.indexOf('.test.js') === -1
     );
   })
-  .forEach(file => {
+  .forEach((file: any) => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
@@ -39,4 +38,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+export default db;
