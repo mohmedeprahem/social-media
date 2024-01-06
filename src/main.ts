cdbb8afda3a07956792de8ddd428f1b3e9a9753c
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-06 12:32:16 +0200

    feat: :sparkles: init swagger feature

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle(process.env.SWAGGER_TITLE || 'Social Media API')
+    .setDescription(process.env.SWAGGER_DESCRIPTION || 'Social Media API Documentation')
+    .setVersion('1.0')
+    .addTag('APIs')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
