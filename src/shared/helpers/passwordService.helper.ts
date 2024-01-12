9b2689d29187f60967dbaebf7b38937a90272929
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-12 15:45:34 +0200

    feat: :sparkles: [BOOT-89] Create new helper file "passwordService

---
+++ src/shared/helpers/passwordService.helper.ts
@@ -0,0 +1,21 @@
+import * as bcrypt from 'bcrypt';
+
+export class PasswordService {
+  private async generateSalt(): Promise<string> {
+    return await bcrypt.genSalt(10);
+  }
+
+  public async hashPassword(password: string): Promise<string> {
+    const salt = await this.generateSalt();
+    return await bcrypt.hash(password, salt);
+  }
+
+  public async comparePassword(
+    password: string,
+    correctPassword: string,
+  ): Promise<boolean> {
+    return (await bcrypt.compare(password, correctPassword))
+      ? true
+      : false;
+  }
+}
