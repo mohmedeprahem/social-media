e6d1ecb2541cce7366443ae7396d65589df7b34c
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-13 04:54:48 +0200

    feat: :sparkles: [BOOT-89] Create generic-exception filter file

---
+++ src/shared/filters/generic-exception.filter.ts
@@ -0,0 +1,25 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class GenericExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+
+    response.status(status).json({
+      success: false,
+      status,
+      message: 'Internal Server Error',
+    });
+  }
+}
