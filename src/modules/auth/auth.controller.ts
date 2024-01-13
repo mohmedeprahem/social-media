70e285b3b77445e384cc29168d172aa6db81b16c
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-13 06:01:44 +0200

    [BOOT-89] ✨ Refactor AuthController with exception handling, Swagger tags, and API versioning
    - Exception Handling: The CreateUserExceptionFilter is now used to centrally handle any errors thrown during user creation. This improves code organization and error reporting.
    - Swagger Integration: Added ApiTags to the AuthController for better API documentation and organization within Swagger.
    - API Versioning: Introduced api/v1/auth as the endpoint prefix to denote API versioning and prevent potential conflicts with future versions.
    - Response Handling: The controller now returns more detailed success/failure responses with specific status codes and messages like "User created successfully" or "Server error". This provides clearer feedback to the client.
    Code Organization: Removed the - redundant try-catch block and switched to the UseFilters decorator for cleaner exception handling.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,30 +1,30 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseFilters } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto.dto';
 import { UserService } from './auth.service';
 import { ApiTags, ApiBody } from '@nestjs/swagger';
+import { CreateUserExceptionFilter } from 'src/shared/filters/create-user-exception.filter';
 
 @ApiTags('User')
 @Controller('api/v1/auth')
-
 export class AuthController {
-  constructor(private readonly _userService : UserService) {}
-  @Post("/register")
+  constructor(private readonly _userService: UserService) {}
+
+  @Post('/register')
   @ApiBody({
     type: CreateUserDto,
     description: 'Json structure for user object',
- })
-  async register(@Body() body : CreateUserDto) : Promise<object> {
-    try {
+  })
+  @UseFilters(CreateUserExceptionFilter)
+  async register(@Body() body: CreateUserDto) {
+    const user = await this._userService.createUser(body);
 
-      const user = await this._userService.createUser(body);
-
-      return user;
-    } catch (error) {
+    if (user.success) {
       return {
         success: true,
-        status: 500,
-        message: "Server error",
-      }
+        status: 200,
+        message: 'User created successfully',
+        isOTPSent: user.isOTPSent,
+      };
     }
   }
 }
