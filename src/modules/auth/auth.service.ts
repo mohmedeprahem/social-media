a4e8619ed111839ce40f87814381b1f620e93ade
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-13 05:36:26 +0200

    [BOOT-89] Enhance user creation with email verification and error handling
    Key improvements:
    
    Email verification:
    - Checks for existing users with the same email.
    - Resends OTP and updates existing user if found and not verified.
    - Throws a CreateUserError with a flag indicating update or conflict.
    Error handling:
    - Uses HttpException for more specific error responses.
    - Introduces CreateUserError for custom error handling.
    Mail service integration:
    - Sends OTP email for verification.
    Return values:
    - Returns isOTPSent flag for clarity.
    Code organization:
    - Removes redundant success/failure checks.
    - Refactors code for better readability.

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,55 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto.dto';
 import { baseResponseDto } from 'src/shared/dto/baseResponse.dto';
 import { UserRepository } from '../../repositories/user.repository';
-import  { plainToClass }  from 'class-transformer'
+import { plainToClass } from 'class-transformer';
 import { User } from '../../database/models/User.entity';
 import { PasswordService } from '../../shared/helpers/passwordService.helper';
+import { MailService } from '../mail/mail.service';
+import { CreateUserError } from 'src/shared/errors/create-user-error';
 
 @Injectable()
 export class UserService {
   constructor(
     private readonly _userRepository: UserRepository,
-    private readonly _passwordService: PasswordService
-    ) {}
+    private readonly _passwordService: PasswordService,
+    private readonly _mailService: MailService,
+  ) {}
 
-  async createUser(createUserDto: CreateUserDto): Promise<baseResponseDto> {
-    try {
-      const user = plainToClass(User, createUserDto);
+  async createUser(createUserDto: CreateUserDto) {
+    const { email, password } = createUserDto;
+    const user = plainToClass(User, createUserDto);
 
-      // Hash password
-      user.password = await this._passwordService.hashPassword(user.password);
+    // Hash password
+    user.password = await this._passwordService.hashPassword(password);
 
-      // Create OTP code
-      user.otpCode = Math.floor(100000 + Math.random() * 900000);
+    // Create OTP code
+    user.otpCode = Math.floor(100000 + Math.random() * 900000);
 
-      // Set OTP creation date
-      user.otpCreatedAt = new Date();
+    // Set OTP creation date
+    user.otpCreatedAt = new Date();
 
-      const createdUser = await this._userRepository.create(user.dataValues);
+    // Check if email is already in use
+    const existingUser = await this._userRepository.findByEmail(email);
 
-      if(createdUser) {
-        // Send OTP
-
-        return {
-          success: true,
-          status: 201,
-          message: 'User created successfully',
-        };
-      } else {
-        console.log(user)
-        return {
-          success: false,
-          status: 400,
-          message: 'Error creating user',
-        };
+    if (existingUser) {
+      if (existingUser.isVerified) {
+        throw new CreateUserError('User already existed', false);
       }
-    } catch (error) {
-      return {
-        success: false,
-        status: 500,
-        message: `Error creating user: ${error.message}`,
-      };
+
+      const userUpdated = await this._userRepository.updateUserById(
+        user.dataValues,
+      );
+
+      if (!userUpdated) {
+        throw new Error('Error updating user');
+      }
+
+      // Send OTP email
+      await this._mailService.sendUserConfirmation(
+        user,
+        user.otpCode.toString(),
+      );
+
+      throw new CreateUserError('User already existed', true);
     }
+
+    const userCreated = await this._userRepository.create(user.dataValues);
+
+    if (!userCreated) {
+      throw new Error('Error creating user');
+    }
+
+    // Send OTP email
+    await this._mailService.sendUserConfirmation(user, user.otpCode.toString());
+
+    return {
+      success: true,
+      isOTPSent: true,
+    };
   }
 }
