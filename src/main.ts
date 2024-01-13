011043e152cb9f37510c08a4e4626dc2c766fb77
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-13 05:19:16 +0200

    feat: :sparkles: [BOOT-89] Init Generic Exception Filter in main file

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { GenericExceptionFilter } from './shared/filters/generic-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle(process.env.SWAGGER_TITLE || 'Social Media API')
-    .setDescription(process.env.SWAGGER_DESCRIPTION || 'Social Media API Documentation')
+    .setDescription(
+      process.env.SWAGGER_DESCRIPTION || 'Social Media API Documentation',
+    )
     .setVersion('1.0')
     .addTag('APIs')
     .build();
@@ -15,8 +18,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.useGlobalPipes(new ValidationPipe({transform: true}));
-
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new GenericExceptionFilter());
 
   await app.listen(3000);
 }
