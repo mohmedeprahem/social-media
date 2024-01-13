f9eeb20064ee4933d3e0a1bcbcc4a32fd227a898
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-13 06:14:35 +0200

    [BOOT-89] :recycle: Refactor module structure and introduce MailModule
    - Updated the module structure by importing the MailModule in addition to the existing SequelizeModule.
    - This change enhances the functionality and allows for better integration of email-related features.
    - The affected modules include imports, controllers, providers, and exports.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -5,15 +5,12 @@ import { UserRepository } from '../../repositories/user.repository';
 import { PasswordService } from '../../shared/helpers/passwordService.helper';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from '../../database/models/User.entity';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
-  controllers: [ AuthController ],
-  providers: [
-    UserRepository,
-    UserService,
-    PasswordService
-  ],
-  exports: [SequelizeModule]
+  imports: [SequelizeModule.forFeature([User]), MailModule],
+  controllers: [AuthController],
+  providers: [UserRepository, UserService, PasswordService],
+  exports: [SequelizeModule],
 })
 export class AuthModule {}
