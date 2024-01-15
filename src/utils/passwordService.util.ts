d283372a1c215558de7abf9a5ad9c10574b81ef1
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-15 02:04:40 +0200

    refactor: :building_construction: [BOOT-89] Remove method from PasswordService
    - Remove GenerateSelt and ComparePassword Methods

--- src/utils/passwordService.util.ts
+++ src/utils/passwordService.util.ts
@@ -1,19 +1,8 @@
 import * as bcrypt from 'bcrypt';
 
 export class PasswordService {
-  private async generateSalt(): Promise<string> {
-    return await bcrypt.genSalt(10);
-  }
-
   public async hashPassword(password: string): Promise<string> {
-    const salt = await this.generateSalt();
+    const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
-  }
-
-  public async comparePassword(
-    password: string,
-    correctPassword: string,
-  ): Promise<boolean> {
-    return (await bcrypt.compare(password, correctPassword)) ? true : false;
   }
 }
