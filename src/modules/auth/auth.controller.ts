1869fd3e4c4b944641e9e636b0d98ce5d85d57e1
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-16 00:36:04 +0200

    feat: :sparkles: [BOOT-90] Create verify Account api

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/createUserDto.dto';
 import { UserService } from './auth.service';
 import { ApiTags, ApiBody } from '@nestjs/swagger';
 import { CreateUserExceptionFilter } from 'src/shared/filters/create-user-exception.filter';
+import { VerifyUserDto } from './dto/verifyUserDto.dto';
 
 @ApiTags('User')
 @Controller('api/v1/auth')
@@ -26,5 +27,20 @@ export class AuthController {
         isOTPSent: user.isOTPSent,
       };
     }
+  }
+
+  @Post('/verify')
+  @ApiBody({
+    type: VerifyUserDto,
+    description: 'Json structure for user object',
+  })
+  async verifyAccount(@Body() body: VerifyUserDto) {
+    await this._userService.verifyAccount(body);
+
+    return {
+      success: true,
+      status: 200,
+      message: 'User verified successfully',
+    };
   }
 }
