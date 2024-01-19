a91a927241b1d87fd8852e136e73c6596ca942d7
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-19 10:35:52 +0200

    feat: :sparkles: [BOOT-90] Add refreshToken in database using migration

--- src/database/migrations/20231226161844-create_users_table.js
+++ src/database/migrations/20231226161844-create_users_table.js
@@ -2,7 +2,7 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     return queryInterface.createTable('users', {
       id: {
         allowNull: false,
@@ -75,7 +75,7 @@ module.exports = {
     })
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     return queryInterface.dropTable('users')
   }
 };
