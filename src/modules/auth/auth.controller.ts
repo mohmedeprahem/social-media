147b34cf52f90a2be434986638e36661b32add28
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-22 15:55:10 +0200

    feat: :sparkles: [BOOT-92] Add login API

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,9 @@
 import { Controller, Get, Post, Body, UseFilters, Res } from '@nestjs/common';
-import { CreateUserDto } from './dto/createUserDto.dto';
 import { UserService } from './auth.service';
 import { ApiTags, ApiBody } from '@nestjs/swagger';
 import { CreateUserExceptionFilter } from 'src/shared/filters/create-user-exception.filter';
-import { VerifyUserDto } from './dto/verifyUserDto.dto';
 import { CookieService, HeaderService } from 'src/utils';
+import { LoginDto, VerifyUserDto, CreateUserDto } from './dto';
 
 @ApiTags('User')
 @Controller('api/v1/auth')
@@ -46,6 +45,24 @@ export class AuthController {
       success: true,
       status: 200,
       message: 'User verified successfully',
+    });
+  }
+
+  @Post('/login')
+  @ApiBody({
+    type: LoginDto,
+  })
+  async login(@Body() body: LoginDto, @Res() res) {
+    const jwtTokens = await this._userService.login(body);
+
+    CookieService.setRefreshTokenCookie(res, jwtTokens.refreshToken);
+
+    HeaderService.setJwtHeader(res, jwtTokens.accessToken);
+
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: 'User logged in successfully',
     });
   }
 }
