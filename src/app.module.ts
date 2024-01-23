5983ce99512f03c94e61ef32e3d43cad4cf2c413
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-23 22:11:28 +0200

    feat: :sparkles: [BOOT-112] Make AtGuard class global

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { User } from './database/models/User.entity';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { KeysService } from './utils/keysService.util';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AtGuard } from './common/guards';
 
 @Module({
   imports: [
@@ -17,6 +19,11 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+  ],
 })
 export class AppModule {}
