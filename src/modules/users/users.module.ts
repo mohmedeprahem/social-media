777d1e251b175b6d73625e27ef55c7d1a1fa9fdb
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-25 17:44:46 +0200

    chore: :sparkles: [BOOT-101] Enhance UsersModule with Sequelize integration
    - Added SequelizeModule.forFeature to import User and UsersFollowing entities in UsersModule. - Integrated UserFollowingRepository and UserRepository into the module's providers. - Exported SequelizeModule for potential use in other modules.

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersFollowing } from 'src/database/models/UsersFollowing.entity';
+import { User } from 'src/database/models/User.entity';
+import {
+  UserFollowingRepository,
+  UserRepository,
+} from 'src/database/repositories';
 
 @Module({
+  imports: [SequelizeModule.forFeature([User, UsersFollowing])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UserFollowingRepository, UserRepository],
+  exports: [SequelizeModule],
 })
 export class UsersModule {}
