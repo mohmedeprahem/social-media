8be7669e9b65e7d8d1212295414c56a7de6a13ad
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-25 17:42:35 +0200

    feat: :sparkles: [BOOT-101] Add followUser method in UsersService

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
+import { UsersFollowing } from 'src/database/models/UsersFollowing.entity';
+import {
+  UserRepository,
+  UserFollowingRepository,
+} from 'src/database/repositories';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    private readonly _userRepository: UserRepository,
+    private readonly _userFollowingRepository: UserFollowingRepository,
+  ) {}
+
+  async followUser(userUuid: string, targetUserUuid: string) {
+    const targetUser = await this._userRepository.findUser({
+      uuid: targetUserUuid,
+    });
+
+    if (!targetUser) {
+      throw new NotFoundException('User not found');
+    }
+
+    const user = await this._userRepository.findUser({
+      uuid: userUuid,
+    });
+
+    if (!user) {
+      throw new HttpException(null, 500);
+    }
+
+    const userFollowing =
+      await this._userFollowingRepository.findOneUsersFollowing({
+        userId: user.id,
+        followingUserId: targetUser.id,
+      });
+
+    if (userFollowing) {
+      await this._userFollowingRepository.delete(userFollowing);
+    } else {
+      await this._userFollowingRepository.create(
+        new UsersFollowing({
+          userId: user.id,
+          followingUserId: targetUser.id,
+        }),
+      );
+    }
+  }
+}
