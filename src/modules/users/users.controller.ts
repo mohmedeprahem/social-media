35bb11b76736ebd15826bb04fd39ff1f11eb01ba
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-25 17:43:20 +0200

    feat: :sparkles: [BOOT-101] Add followUser Post api

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Req, Res } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { FollowUserDto } from './dto';
+import { IGetUserAuthInfoRequest } from 'src/common/interfaces/IGetUserAuthInfoRequest.interface';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('api/v1/users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly _usersService: UsersService) {}
+
+  @Post('follow')
+  @ApiBody({
+    type: FollowUserDto,
+    description: 'Json structure for user object',
+  })
+  async followUser(
+    @Body() body: FollowUserDto,
+    @Req() req: IGetUserAuthInfoRequest,
+    @Res() res,
+  ) {
+    await this._usersService.followUser(req.user.sub, body.targetUserUuid);
+
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      message: 'success',
+    });
+  }
+}
