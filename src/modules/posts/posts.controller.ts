b092e3dfd54548748314f5d0c691a7f201943142
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-28 23:21:53 +0200

    feat: :sparkles: [BOOT-95] Add getOnePost GET api

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post, Body, Req, Res } from '@nestjs/common';
+import { Controller, Post, Body, Req, Res, Get, Param } from '@nestjs/common';
 import { IGetUserAuthInfoRequest } from 'src/common/interfaces/IGetUserAuthInfoRequest.interface';
 import { ApiBody } from '@nestjs/swagger';
 import { CreatePostDto } from './dto/createPost.dto';
 import { PostsService } from './posts.service';
+import { LikesService } from '../likes/likes.service';
 
 @Controller('api/v1/posts')
 export class PostsController {
-  constructor(private _postsService: PostsService) {}
+  constructor(
+    private _postsService: PostsService,
+    private _likesService: LikesService,
+  ) {}
 
   @Post()
   @ApiBody({
@@ -27,6 +31,39 @@ export class PostsController {
       status: 201,
       message: 'Post created successfully',
       postID: newPost.id,
+    });
+  }
+
+  @Get(':id')
+  async getOnePost(
+    @Req() req: IGetUserAuthInfoRequest,
+    @Res() res,
+    @Param('id') id: number,
+  ) {
+    const post = await this._postsService.getOnePost(req.user.sub, id);
+
+    const isLiked = await this._likesService.userLikesPost(
+      req.user.sub,
+      post.id,
+    );
+
+    const postResponse = {
+      id: post.id,
+      description: post.description,
+      numberOfLikes: post.likesCounter,
+      numberOfComments: post.commentsCounter,
+      isLiked: isLiked,
+      createdAt: post.createdAt,
+      user: {
+        fullName: post.user.fullName,
+        UUID: post.user.uuid,
+      },
+    };
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      message: 'Post fetched successfully',
+      postResponse,
     });
   }
 }
