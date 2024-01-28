d059b1ca7fe843d83a9e4abb64cf81007ba2fafe
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-28 20:43:42 +0200

    feat: :sparkles: [BOOT-130] Add Create new post API

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Req, Res } from '@nestjs/common';
+import { IGetUserAuthInfoRequest } from 'src/common/interfaces/IGetUserAuthInfoRequest.interface';
+import { ApiBody } from '@nestjs/swagger';
+import { CreatePostDto } from './dto/createPost.dto';
+import { PostsService } from './posts.service';
 
-@Controller('posts')
-export class PostsController {}
+@Controller('api/v1/posts')
+export class PostsController {
+  constructor(private _postsService: PostsService) {}
+
+  @Post()
+  @ApiBody({
+    type: CreatePostDto,
+  })
+  async createPost(
+    @Body() body: CreatePostDto,
+    @Req() req: IGetUserAuthInfoRequest,
+    @Res() res,
+  ) {
+    const newPost = await this._postsService.createPost(
+      req.user.sub,
+      body.description,
+    );
+
+    return res.status(201).json({
+      success: true,
+      status: 201,
+      message: 'Post created successfully',
+      postID: newPost.id,
+    });
+  }
+}
