840452b46f325cbecd96ee24367866b0f854d296
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-28 20:44:55 +0200

    feat: :sparkles: [BOOT-130] Import Sequelize, PostRepository and UserRepository in Post module

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from 'src/database/models/User.entity';
+import { Post } from 'src/database/models/Post.entity';
+import { PostRepository, UserRepository } from '../../database/repositories';
 
 @Module({
-  imports: [],
+  imports: [SequelizeModule.forFeature([User, Post])],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, PostRepository, UserRepository],
 })
 export class PostsModule {}
