23421f60b159564dd900049860e8bdadcf591433
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-28 23:19:33 +0200

    feat: :sparkles: [BOOT-95] Create Like Service file with userLikesPost method

---
+++ src/modules/likes/likes.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { UserRepository } from 'src/database/repositories';
+import { LikeRepository } from 'src/database/repositories/like.repository';
+
+@Injectable()
+export class LikesService {
+  constructor(
+    private readonly _likesRepository: LikeRepository,
+    private readonly _userRepository: UserRepository,
+  ) {}
+
+  async userLikesPost(userUuid: string, postId: number): Promise<boolean> {
+    const user = await this._userRepository.findUser({
+      uuid: userUuid,
+    });
+    if (!user) {
+      throw new Error();
+    }
+
+    return await this._likesRepository.IsUserLikedPost(user.id, postId);
+  }
+}
