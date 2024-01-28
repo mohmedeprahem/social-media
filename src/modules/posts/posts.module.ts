bf9b01f74fbe52cee7d46a3be550b44c7fbf35b9
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-28 23:22:57 +0200

    refactor: :sparkles: [BOOT-95] Import PostsLike entity, LikeService and LikeRepository

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -4,11 +4,23 @@ import { PostsController } from './posts.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from 'src/database/models/User.entity';
 import { Post } from 'src/database/models/Post.entity';
-import { PostRepository, UserRepository } from '../../database/repositories';
+import {
+  PostRepository,
+  UserRepository,
+  LikeRepository,
+} from '../../database/repositories';
+import { LikesService } from '../likes/likes.service';
+import { PostsLike } from 'src/database/models/PostsLike.entity';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User, Post])],
+  imports: [SequelizeModule.forFeature([User, Post, PostsLike])],
   controllers: [PostsController],
-  providers: [PostsService, PostRepository, UserRepository],
+  providers: [
+    PostsService,
+    PostRepository,
+    UserRepository,
+    LikesService,
+    LikeRepository,
+  ],
 })
 export class PostsModule {}
