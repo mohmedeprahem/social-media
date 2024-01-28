13c048bc86823000d05ff98e21a22b4c8f34220e
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-28 23:15:57 +0200

    fix: :bug: [BOOT-95] Customize PostsLike table name and remove some column
    - Change table name to postsLikes. - Change timestamps to false. - remove Id column

--- src/database/models/PostsLike.entity.ts
+++ src/database/models/PostsLike.entity.ts
@@ -1,19 +1,28 @@
-import { Table, Column, Model, ForeignKey, BelongsTo } from 'sequelize-typescript';
+import {
+  Table,
+  Column,
+  Model,
+  ForeignKey,
+  BelongsTo,
+} from 'sequelize-typescript';
 import { Post } from './Post.entity';
 import { User } from './User.entity';
 
-@Table
+@Table({ tableName: 'postsLikes', timestamps: false })
 export class PostsLike extends Model {
-
   @ForeignKey(() => User)
-  @Column
+  @Column({
+    primaryKey: true,
+  })
   userId: number;
 
   @BelongsTo(() => User)
   user: User;
 
   @ForeignKey(() => Post)
-  @Column
+  @Column({
+    primaryKey: true,
+  })
   postId: number;
 
   @BelongsTo(() => Post)
