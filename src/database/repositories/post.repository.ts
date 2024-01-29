537ea63e1229fd8ea8084d1546bcdebe435ef51b
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-29 13:04:18 +0200

    feat: :sparkles: [BOOT-100] Add findPostsWithIsLiked method in post repository
    - Implement findPostsWithIsLiked method. - Inject PostLike entity.

--- src/database/repositories/post.repository.ts
+++ src/database/repositories/post.repository.ts
@@ -2,10 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Post } from '../models/Post.entity';
 import { Includeable } from 'sequelize/types';
+import { PostsLike } from '../models/PostsLike.entity';
+import sequelize from 'sequelize';
 
 @Injectable()
 export class PostRepository {
-  constructor(@InjectModel(Post) private readonly postModel: typeof Post) {}
+  constructor(
+    @InjectModel(Post) private readonly postModel: typeof Post,
+    @InjectModel(PostsLike) private readonly postLikeModel: typeof PostsLike,
+  ) {}
 
   async create(post: Post): Promise<Post> {
     const newPost = await this.postModel.create({
@@ -22,5 +27,39 @@ export class PostRepository {
     return this.postModel.findByPk(id, {
       include: includeOptions,
     });
+  }
+
+  async findPostsWithIsLiked(userId: number, targetUserId: number) {
+    const posts = await this.postModel.findAll({
+      attributes: [
+        'id',
+        'description',
+        'likesCounter',
+        'commentsCounter',
+        'createdAt',
+        'updatedAt',
+        [
+          sequelize.literal(
+            'CASE WHEN "postLikes"."userId" IS NOT NULL THEN true ELSE false END',
+          ),
+          'isLiked',
+        ],
+      ],
+      where: {
+        userId: targetUserId,
+      },
+      include: [
+        {
+          model: this.postLikeModel,
+          attributes: ['userId'],
+          where: {
+            userId: userId,
+          },
+          required: false,
+        },
+      ],
+    });
+
+    return posts;
   }
 }
