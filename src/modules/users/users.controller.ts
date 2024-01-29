7de7f512521de0c7689fabd18ac1ff87f7e4993e
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-29 13:06:48 +0200

    feat: :sparkles: [BOOT-100] Add getUserPosts API

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Post, Body, Req, Res } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Req,
+  Res,
+  Get,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { FollowUserDto } from './dto';
 import { IGetUserAuthInfoRequest } from 'src/common/interfaces/IGetUserAuthInfoRequest.interface';
 import { ApiBody } from '@nestjs/swagger';
+import { PostsService } from '../posts/posts.service';
 
 @Controller('api/v1/users')
 export class UsersController {
-  constructor(private readonly _usersService: UsersService) {}
+  constructor(
+    private readonly _usersService: UsersService,
+    private readonly _postsService: PostsService,
+  ) {}
 
   @Post('follow')
   @ApiBody({
@@ -24,6 +37,37 @@ export class UsersController {
       success: true,
       status: 200,
       message: 'success',
+    });
+  }
+
+  @Get(':id/posts')
+  async getUserPosts(
+    @Req() req: IGetUserAuthInfoRequest,
+    @Res() res,
+    @Param('id') id: number,
+  ) {
+    const posts = await this._postsService.getUserPosts(req.user.sub, id);
+
+    if (!posts || posts.length === 0) {
+      throw new NotFoundException('not found');
+    }
+
+    const successResponse = {
+      success: true,
+      status: 200,
+      message: 'success',
+      ...posts.map((post) => ({
+        id: post.id,
+        description: post.description,
+        numberOfLikes: post.likesCounter,
+        numberOfComments: post.commentsCounter,
+        isLiked: post.isLiked,
+        createdAt: post.createdAt,
+      })),
+    };
+
+    return res.status(200).json({
+      successResponse,
     });
   }
 }
