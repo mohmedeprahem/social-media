0c3d874fd96b980f3597657d83e47d1677327e0b
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-29 13:10:54 +0200

    refactor: :recycle: [BOOT-100] Import some entities and providers in users module
    - Import PostsLike and Post entity. - Import Post repository and Post service.

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,14 +5,26 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersFollowing } from 'src/database/models/UsersFollowing.entity';
 import { User } from 'src/database/models/User.entity';
 import {
+  PostRepository,
   UserFollowingRepository,
   UserRepository,
 } from 'src/database/repositories';
+import { PostsLike } from 'src/database/models/PostsLike.entity';
+import { PostsService } from '../posts/posts.service';
+import { Post } from 'src/database/models/Post.entity';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User, UsersFollowing])],
+  imports: [
+    SequelizeModule.forFeature([User, UsersFollowing, PostsLike, Post]),
+  ],
   controllers: [UsersController],
-  providers: [UsersService, UserFollowingRepository, UserRepository],
+  providers: [
+    UsersService,
+    UserFollowingRepository,
+    UserRepository,
+    PostRepository,
+    PostsService,
+  ],
   exports: [SequelizeModule],
 })
 export class UsersModule {}
