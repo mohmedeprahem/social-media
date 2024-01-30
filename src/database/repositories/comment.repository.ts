65925c5a43669bbcf77f25d9f742a78f93cf6fe9
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-30 18:06:57 +0200

    feat: :sparkles: [BOOT-96] Add getCommentsForPost method in repository

--- src/database/repositories/comment.repository.ts
+++ src/database/repositories/comment.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Comment } from '../models/Comment.entity';
+import { Includeable } from 'sequelize';
 
 @Injectable()
 export class CommentRepository {
@@ -11,6 +12,24 @@ export class CommentRepository {
   async createComment(comment: Comment) {
     await this.CommentModel.create({
       ...comment.dataValues,
+    });
+  }
+
+  async getCommentsForPost(
+    postId: number,
+    include: string[] = [],
+    pageNumber = 1,
+  ) {
+    const includeOptions: Includeable[] = include.map((assoc) => ({
+      model: this.CommentModel.associations[assoc].target,
+    }));
+
+    return await this.CommentModel.findAll({
+      where: {
+        postId,
+      },
+      include: includeOptions,
+      offset: (pageNumber - 1) * 10,
     });
   }
 }
