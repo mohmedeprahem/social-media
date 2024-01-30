9f8a291471fef6a5c57a402aa06581e49068bd61
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-30 21:57:02 +0200

    refactor: :recycle: Make port of server is optional

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new GenericExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
