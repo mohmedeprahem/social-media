077501b592cafcc674823bf05be38284194ad028
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-30 17:28:40 +0200

    feat: :sparkles: [BOOT-97] Add createComment method in comment service

--- src/modules/comments/comments.service.ts
+++ src/modules/comments/comments.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  UserRepository,
+  CommentRepository,
+  PostRepository,
+} from 'src/database/repositories';
+import { CreateCommentDto } from './dto';
+import { plainToClass } from 'class-transformer';
+import { Comment } from 'src/database/models/Comment.entity';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    private readonly _userRepository: UserRepository,
+    private readonly _commentRepository: CommentRepository,
+    private readonly _postRepository: PostRepository,
+  ) {}
+
+  async createComment(
+    userUuid: string,
+    postId: number,
+    body: CreateCommentDto,
+  ) {
+    const user = await this._userRepository.findUser({
+      uuid: userUuid,
+    });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    const post = await this._postRepository.findOneById(postId);
+
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    const comment = plainToClass(Comment, body);
+
+    comment.userId = user.id;
+    comment.postId = post.id;
+
+    await this._commentRepository.createComment(comment);
+  }
+}
