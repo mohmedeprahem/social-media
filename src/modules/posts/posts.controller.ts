d67ca0048eb9527f1e356ab5088bda7fb0bfa230
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-30 15:54:24 +0200

    feat: :sparkles: [BOOT-94] Create likePost PATCH API

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Post, Body, Req, Res, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Req,
+  Res,
+  Get,
+  Param,
+  Patch,
+} from '@nestjs/common';
 import { IGetUserAuthInfoRequest } from 'src/common/interfaces/IGetUserAuthInfoRequest.interface';
 import { ApiBody } from '@nestjs/swagger';
 import { CreatePostDto } from './dto/createPost.dto';
@@ -64,6 +73,22 @@ export class PostsController {
       status: 200,
       message: 'Post fetched successfully',
       postResponse,
+    });
+  }
+
+  @Patch(':id/like')
+  async likePost(
+    @Req() req: IGetUserAuthInfoRequest,
+    @Res() res,
+    @Param('id') id: number,
+  ) {
+    const post = await this._postsService.getOnePost(req.user.sub, id);
+    await this._likesService.likePost(req.user.sub, post.id);
+
+    return res.status(200).json({
+      success: true,
+      status: 200,
+      message: 'Post liked successfully',
     });
   }
 }
