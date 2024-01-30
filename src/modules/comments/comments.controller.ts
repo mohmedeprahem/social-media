75262f61d8ee921860da46f9bf00876c10e83872
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-30 17:30:56 +0200

    feat: :sparkles: [BOOT-97] Add createComment POST API

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Req, Res, Param } from '@nestjs/common';
+import { IGetUserAuthInfoRequest } from 'src/common/interfaces';
+import { CommentsService } from './comments.service';
+import { CreateCommentDto } from './dto';
 
-@Controller('posts/:postId/comments')
-export class CommentsController {}
+@Controller('api/v1/posts/:postId/comments')
+export class CommentsController {
+  constructor(private readonly _commentsService: CommentsService) {}
+
+  @Post()
+  async createComment(
+    @Req() req: IGetUserAuthInfoRequest,
+    @Res() res,
+    @Param('postId') postId,
+    @Body() body: CreateCommentDto,
+  ) {
+    const comment = await this._commentsService.createComment(
+      req.user.sub,
+      postId,
+      body,
+    );
+
+    return res.status(201).json({
+      success: true,
+      status: 201,
+      message: 'Comment created successfully',
+    });
+  }
+}
