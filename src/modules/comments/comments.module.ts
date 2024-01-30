86ea3fc3b71fd092c42b699e94cb8e7e4f34af3f
mohmedeprahem <m.eprahem.168@gmail.com>
2024-01-30 17:32:52 +0200

    refactor: :recycle: [BOOT-97] Import SequelizeModule, CommentRepository, PostRepository in comment module

--- src/modules/comments/comments.module.ts
+++ src/modules/comments/comments.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
+import { CommentsController } from './comments.controller';
+import { CommentsService } from './comments.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from 'src/database/models/User.entity';
+import { Comment } from 'src/database/models/Comment.entity';
+import { Post } from 'src/database/models/Post.entity';
+import { PostsLike } from 'src/database/models/PostsLike.entity';
+import {
+  UserRepository,
+  CommentRepository,
+  PostRepository,
+} from 'src/database/repositories';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [SequelizeModule.forFeature([User, Comment, Post, PostsLike])],
+  controllers: [CommentsController],
+  providers: [
+    CommentsService,
+    UserRepository,
+    CommentRepository,
+    PostRepository,
+  ],
 })
-export class PostsModule {}
+export class CommentsModule {}
