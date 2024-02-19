99b1ca1f8ff33973ff7585707809e71bf26dc95e
mohmedeprahem <m.eprahem.168@gmail.com>
2024-02-19 10:32:23 +0200

    refactor: :recycle: [BOOT-102] Enhance security of 'UpdateEmailRequestDto'

--- src/modules/users/dto/updateEmailRequest.dto.ts
+++ src/modules/users/dto/updateEmailRequest.dto.ts
@@ -1,8 +1,11 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsEmail } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateEmailRequestDto {
   @ApiProperty()
-  @IsNotEmpty()
+  @IsNotEmpty({
+    message: 'Please enter a new email address',
+  })
+  @IsEmail()
   newEmail: string;
 }
